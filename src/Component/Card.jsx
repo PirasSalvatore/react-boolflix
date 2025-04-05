08e2d4b67bb724a393cc1ca0fd005b444de2f62b
PirasSalvatore <torep.92@gmail.com>
2025-04-05 14:07:35 +0200

    add img on card

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,13 +1,22 @@
 
 import FlagLanguage from "./FlagLanguage"
 
+const base_url_img = "https://image.tmdb.org/t/p/"
+const width_url_img = "w185"
+
 export default function Card({ item }) {
 
     return (
         <div className="col">
             <div className="card">
-                <div className="card-body">
-                    <ul>
+                <div className="card-body " >
+                    {
+                        item.poster_path ?
+                            (<img className="card-img-top" src={base_url_img + width_url_img + item.poster_path} alt={item.original_title || item.original_name} />)
+                            :
+                            (<img className="card-img-top" width="154px" src="https://www.avislivorno.it/wp-content/uploads/2016/01/ciak.png" alt="" />)
+                    }
+                    <ul className="list-unstyled">
                         <li>
                             {item.title || item.name}
                         </li>
--- src/Component/SectionList.jsx
+++ src/Component/SectionList.jsx
@@ -7,7 +7,7 @@ export default function SectionList({ type, list }) {
     return (
         <section >
             {list.length !== 0 && (<h1>{type}</h1>)}
-            <div className="row">
+            <div className="row row-cols-2 row-cols-md-3 row-cols-lg-4 gap-2">
                 {
                     list.map(item => {
                         return (
